8f30d7b8c46585bf21534f03aabadf22261f2f3d
Julia Costa <ramosjulia716@gmail.com>
2022-10-20 20:59:26 -0300

    Injeção de dependencias

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -10,31 +11,11 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
-  constructor() { }
+  constructor(private courseService: CourseService) { 
 
-  ngOnInit(): void {
-    this.courses = [
-      {
-        id: 21,
-        name: 'Angular 14',
-        imageUrl: '/assets/images/angular.png',
-        price: 1399,
-        code: '751A31',
-        duration: 40,
-        rating: 4.5,
-        releaseDate: '21/01/2022'
-      },
-      {
-        id: 6,
-        name: 'Java',
-        imageUrl: '/assets/images/java.png',
-        price: 254,
-        code: '899J89',
-        duration: 32,
-        rating: 3,
-        releaseDate: '23/07/2021'
-      }
-    ]
   }
 
+  ngOnInit(): void {
+    this.courses = this.courseService.retrieveAll();
+  }
 }
